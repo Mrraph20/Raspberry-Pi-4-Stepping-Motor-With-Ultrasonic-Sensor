c385ef51615b7b02d7fc803e37c1c0def6b9b070
Christopher Milian <christophermilian16@gmail.com>
2020-05-09 01:15:55 -0400

    Removed Step Motor printf lines. Cleaned up comments.

--- SteppingMotorWithUltrasonicSensor.c
+++ SteppingMotorWithUltrasonicSensor.c
@@ -18,19 +18,17 @@ const int motorPins[]={1,0,4,6};  // GPIO 18,17,23,25 // Define pins connected t
 const int CCWStep[]={0x01,0x02,0x04,0x08};  // Define power supply order for coil for rotating anticlockwise 
 const int CWStep[]={0x08,0x04,0x02,0x01};   // Define power supply order for coil for rotating clockwise
 
-// As for four phase stepping motor, four steps is a cycle. the function is used to drive the stepping motor clockwise or anticlockwise to take four steps
+// Motor: For four phase step motor, four steps is a cycle. The function is used to drive the stepping motor clockwise or anticlockwise to take four steps
 void moveOnePeriod(int dir,int ms){
     int i=0,j=0;
     for (j=0;j<4;j++){      // Cycle according to power supply order 
-        for (i=0;i<4;i++){  // Assign to each pin, a total of 4 pins
-            if(dir == 1)    // Power supply order clockwise
-                digitalWrite(motorPins[i],(CCWStep[j] == (1<<i)) ? HIGH : LOW);
-            else  // Power supply order anticlockwise
-                digitalWrite(motorPins[i],(CWStep[j] == (1<<i)) ? HIGH : LOW);
-            printf("motorPin %d, %d \n",motorPins[i],digitalRead(motorPins[i]));
+        for (i=0;i<4;i++){  // Assigning to each pin, a total of 4 pins
+            if(dir == 1)    
+                digitalWrite(motorPins[i],(CCWStep[j] == (1<<i)) ? HIGH : LOW);  // Power supply order clockwise
+            else  
+                digitalWrite(motorPins[i],(CWStep[j] == (1<<i)) ? HIGH : LOW);  // Power supply order anticlockwise
         }
-        printf("Step cycle!\n");
-        if(ms<3)  // The delay can not be less than 3ms, otherwise it will exceed speed limit of the motor
+        if(ms<3)  // The delay cannot be less than 3ms, otherwise it will exceed speed limit of the motor
             ms=3;
         delay(ms);
     }
@@ -103,7 +101,7 @@ int main(){
         printf("The distance is : %.2f cm\n",distance);
         delay(500);
     
-        moveSteps(1,3,512);  // Rotating 360° clockwise, a total of 2048 steps in a circle, namely, 512 cycles.
+        moveSteps(1,3,512);  // Rotating 360° clockwise, a total of 2048 steps in a circle, namely, 512 cycles. 2048/4 = 512
         delay(1000);
         moveSteps(0,3,512);  // Rotating 360° anticlockwise
         delay(1000);
